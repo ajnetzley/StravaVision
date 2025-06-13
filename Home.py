1e82683b38c298f72f793adf3f203a6a11333494
ajnetzley <anetzley@uw.edu>
2025-06-13 14:04:47 -0500

    First iteration of Streamlit UI

---
+++ Home.py
@@ -0,0 +1,17 @@
+"""
+app.py
+v0.0.1, 6/13/2025
+Author: Alexander Netzley, anetzley@uw.edu
+This module provides the wrapper function for running the StravaVision App.
+"""
+
+# Import packages
+import streamlit as st
+
+# Import user modules
+
+# Setting page formats
+st.set_page_config(layout="wide")
+
+# Title of the app
+st.title('StravaVision')
